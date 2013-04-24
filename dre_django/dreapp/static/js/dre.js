dbc485a846920e9999a64808e61fced7dda701c5
Helder Guerreiro <helder@tretas.org>
2013-04-24 01:37:12 +0100

    Bookmark filter jQuery widgets

---
+++ dre_django/dreapp/static/js/dre.js
@@ -0,0 +1,27 @@
+/***********************************/
+/* Dre suporting JavaScript        */
+/*                                 */
+/* Usage of this code is optional. */
+/***********************************/
+
+
+function dre_filter_setup() {
+    $("#id_start_date").datepicker({
+        dateFormat: "yy-mm-dd",
+        changeMonth: true,
+        changeYear: true
+    });
+    $("#id_end_date").datepicker({
+        dateFormat: "yy-mm-dd",
+        changeMonth: true,
+        changeYear: true
+    });
+    $("#id_tags").multiselect()
+};
+
+
+$(function() {
+    if (is_owner) {
+        dre_filter_setup();
+    };
+});
