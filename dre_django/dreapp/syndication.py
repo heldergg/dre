99600a57a7bd03da3db171d0674abe83822f19eb
Helder Guerreiro <helder@tretas.org>
2013-06-17 09:29:18 +0100

    Configurable number of RSS entries

--- dre_django/dreapp/syndication.py
+++ dre_django/dreapp/syndication.py
@@ -3,6 +3,7 @@
 # Global imports:
 from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 # Local imports
 from models import Document
@@ -11,13 +12,15 @@ from models import Document
 # Latest
 ##
 
+NUMBER_ENTRIES = getattr(settings, 'NUMBER_ENTRIES', 25)
+
 class LatestEntriesFeed(Feed):
     title = 'Diários da República'
     link = "/latest/" # TODO: Make the view corresponding to this URL
     description = 'Modificações e novos documentos acrescentados ao site'
 
     def items(self):
-        return Document.objects.order_by('-timestamp')[:50]
+        return Document.objects.order_by('-timestamp')[:NUMBER_ENTRIES]
 
     def item_title(self, item):
         return item.title()
