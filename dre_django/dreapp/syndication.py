a6d68b8e3b7ba706ced349c76647f6e31bc6762c
Helder Guerreiro <helder@tretas.org>
2015-03-02 20:14:29 +0000

    Make the syndication query aware
    Now we can have sysdication urls in the form:
    
    http://dre.tretas.org/dre/rss/?q=<query>

--- dre_django/dreapp/syndication.py
+++ dre_django/dreapp/syndication.py
@@ -1,29 +1,58 @@
 # -*- coding: utf-8 -*-
 
 # Global imports:
+from datetime import datetime, time
 from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
 # Local imports
+import dreapp.index
 from models import Document
+from djapian.resultset import ResultSet
+
 
 ##
 # Latest
 ##
 
 NUMBER_ENTRIES = getattr(settings, 'NUMBER_ENTRIES', 25)
+SITE_URL = getattr(settings, 'SITE_URL', 'http://dre.tretas.org')
 
 class LatestEntriesFeed(Feed):
     title = 'Diários da República'
-    link = "/latest/" # TODO: Make the view corresponding to this URL
+    link = SITE_URL
     description = 'Modificações e novos documentos acrescentados ao site'
+    ttl = 3600*12
 
-    def items(self):
-        return Document.objects.order_by('-timestamp')[:NUMBER_ENTRIES]
+    def get_object(self, request, *args, **kwargs):
+        return request
+
+    def items(self, request):
+        query = request.GET.get('q', None)
+        if not query:
+            return Document.objects.order_by('-timestamp')[:NUMBER_ENTRIES]
+        else:
+            # Return an rss resulting from a database query
+            indexer = Document.indexer
+            results = ResultSet(indexer, query, parse_query=True).order_by('-date')
+            object_list = []
+            for obj in results[:NUMBER_ENTRIES]:
+                object_list.append(obj.instance)
+
+            return object_list
 
     def item_title(self, item):
         return item.title()
 
     def item_description(self, item):
         return item.note_abrv()
+
+    def item_guid(self, item):
+        return str(item.id)
+
+    def item_author_name(self, item):
+        return item.emiting_body
+
+    def item_pubdate(self, item):
+        return datetime.combine(item.date, time())
