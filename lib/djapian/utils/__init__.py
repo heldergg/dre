1e23adccb6199be91112787c13657a8f8b9205b2
Helder Guerreiro <helder@tretas.org>
2015-12-19 16:12:11 +0000

    Use 'django.apps.apps' to import the index modules

--- lib/djapian/utils/__init__.py
+++ lib/djapian/utils/__init__.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.apps import apps
 
 DEFAULT_WEIGHT = 1
 
@@ -7,8 +8,9 @@ def model_name(model):
 
 def load_indexes():
     from djapian.utils import loading
-    for app in settings.INSTALLED_APPS:
+    for app_config in apps.get_app_configs():
         try:
-            loading.get_module(app, "index")
+            module = app_config.module.__name__
+            loading.get_module(module, "index")
         except loading.NoModuleError:
             pass
