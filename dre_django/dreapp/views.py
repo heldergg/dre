38c671d8d0fce2a392284288adb3c028412a8314
Helder Guerreiro <helder@tretas.org>
2016-12-21 01:10:55 +0000

    Display the PDF warning only until 2016-12-19

--- dre_django/dreapp/views.py
+++ dre_django/dreapp/views.py
@@ -328,7 +328,10 @@ def document_display( request, docid ):
     if request.user.is_authenticated():
         context['show_user_notes'] = get_setting(request.user, 'show_user_notes')
 
-    context['pdf_warning'] = document.date >= datetime.date(2016,3,24)
+    # TODO: retrieve the html text between these dates and remove the
+    # warning altogether
+    context['pdf_warning'] = (document.date >= datetime.date(2016,3,24) and
+                              document.date < datetime.date(2016, 12, 19))
 
     return render_to_response('document_display.html', context,
                 context_instance=RequestContext(request))
